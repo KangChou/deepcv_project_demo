7be8b39a5a0b42183a9b7eecc49944ff811604f1
KangChou <kangchou666@gmail.com>
2022-02-23 10:35:08 +0800

    Add files via upload

---
+++ learning_sets/CM2022/build/CMakeFiles/C_Lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_Lib.dir/library.c.o"
+  "libC_Lib.pdb"
+  "libC_Lib.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_Lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ learning_sets/CM2022/library.h
@@ -0,0 +1,4 @@
+#ifndef C_LIB_LIBRARY_H
+#define C_LIB_LIBRARY_H
+void hello(void);
+#endif //C_LIB_LIBRARY_H
